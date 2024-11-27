f4a8cdbf9637734b0541ce1ca74e96c72cced353
김다온 <128460925+kdaon07@users.noreply.github.com>
2024-11-28 08:21:11 +0900

    temp fixed

--- src/Page/Quiz/Volcano/index.jsx
+++ src/Page/Quiz/Volcano/index.jsx
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { createGlobalStyle } from "styled-components";
+
+import bg from "../../../assets/Background/volcano.svg";
 
 import Chat from '../../../Components/Chat.jsx'
 import Cloud from './Cloud.jsx'
@@ -59,9 +62,27 @@ export default function Volcano() {
 
     return (
         <>
-            <QuizBg $bg={"volcano"}/>
+            <GlobalStyles bg={bg}/>
             <Cloud />
             <Chat Obj={'volcano'} size={120} left={-12} bottom={-5} anime={true} id={id} text={text} map={map} title={3}/>
         </>
     )
 }
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        width: 100vw;
+        height: 100vh; 
+        margin: 0;
+        background-image: ${({ bg }) => `url(${bg})`}; 
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        overflow: hidden;
+        -webkit-user-drag: none;
+        -moz-user-drag: none;
+        -ms-user-drag: none;
+        user-select: none;
+    }
+`;
